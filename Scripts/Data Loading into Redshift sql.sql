408a6dfb958f2e864789469246fea89c13f13465
Muqsit Momin <mamomin78692@gmail.com>
2024-05-14 02:59:17 -0400

    sql changes

--- Scripts/Data Loading into Redshift sql.sql
+++ Scripts/Data Loading into Redshift sql.sql
@@ -1,26 +1,25 @@
-
-COPY BBP.dim_time
+COPY bbp.dim_time
 FROM 's3://cis9440-hw-raw-data/dim_time.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
 
 
-COPY BBP.dim_weather
+COPY bbp.dim_weather
 FROM 's3://cis9440-hw-raw-data/dim_weather.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
 
 
-COPY BBP.dim_event
+COPY bbp.dim_event
 FROM 's3://cis9440-hw-raw-data/dim_event.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
 
 
-COPY BBP.fact_pedestrian
+COPY bbp.fact_pedestrian
 FROM 's3://cis9440-hw-raw-data/fact_pedestrian.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
