9b5a9c08ea3bd051c446dfffbd9522475b295fc4
Muqsit Momin <mamomin78692@gmail.com>
2024-05-13 22:45:37 -0400

    Data nd table loading sql updates

--- Scripts/Data Loading into Redshift sql.sql
+++ Scripts/Data Loading into Redshift sql.sql
@@ -1,22 +1,27 @@
 -- Load data from S3 into Redshift tables
 
 -- Set the role to allow Redshift to access the S3 bucket
-COPY bbp.dim_time FROM 's3://cis9440-hw-raw-data/dim_time.csv'
+COPY bbp.dim_time
+FROM 's3://cis9440-hw-raw-data/dim_time.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
 
-COPY bbp.dim_weather FROM 's3://cis9440-hw-raw-data/dim_weather.csv'
+COPY bbp.dim_weather
+FROM 's3://cis9440-hw-raw-data/dim_weather.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
 
-COPY bbp.dim_event FROM 's3://cis9440-hw-raw-data/dim_event.csv'
+COPY bbp.dim_event
+FROM 's3://cis9440-hw-raw-data/dim_event.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
 
-COPY bbp.fact_pedestrian FROM 's3://cis9440-hw-raw-data/fact_pedestrian.csv'
+COPY bbp.fact_pedestrian
+FROM 's3://cis9440-hw-raw-data/fact_pedestrian.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
+
