242e936cd02e2f9bbbcc019f61154b99cacd254e
Muqsit Momin <mamomin78692@gmail.com>
2024-05-14 01:36:27 -0400

    update data loading sql

--- Scripts/Data Loading into Redshift sql.sql
+++ Scripts/Data Loading into Redshift sql.sql
@@ -1,25 +1,26 @@
--- Load data from S3 into Redshift tables
 
--- Set the role to allow Redshift to access the S3 bucket
-COPY bbp.dim_time
+COPY BBP.dim_time
 FROM 's3://cis9440-hw-raw-data/dim_time.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
 
-COPY bbp.dim_weather
+
+COPY BBP.dim_weather
 FROM 's3://cis9440-hw-raw-data/dim_weather.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
 
-COPY bbp.dim_event
+
+COPY BBP.dim_event
 FROM 's3://cis9440-hw-raw-data/dim_event.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
 CSV;
 
-COPY bbp.fact_pedestrian
+
+COPY BBP.fact_pedestrian
 FROM 's3://cis9440-hw-raw-data/fact_pedestrian.csv'
 IAM_ROLE 'arn:aws:iam::788566477685:role/redshift-role'
 IGNOREHEADER 1
